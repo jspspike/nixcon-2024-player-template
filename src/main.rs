8f7bb608efcf15b457d064bbf3a1c83dc2ba807f
jjohnson <jjohnson@cloudflare.com>
2024-10-26 18:27:13 +0200

    added mult

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
-use axum::{
-    extract::Path, routing::get, Router
-};
+use axum::{extract::Path, routing::get, Router};
 
-async fn handle(
-    Path((a, b)): Path<(u32, u32)>,
-) -> String {
+async fn add(Path((a, b)): Path<(u32, u32)>) -> String {
     (a + b).to_string()
+}
+
+async fn mult(Path((a, b)): Path<(u32, u32)>) -> String {
+    (a * b).to_string()
 }
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }))
-.route("/add/:a/:b", get(handle));
-        ;
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/add/:a/:b", get(add))
+        .route("/mult/:a/:b", get(mult));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
