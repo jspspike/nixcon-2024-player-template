9b49326ea2160622c8361bb3bc023662089b792b
jjohnson <jjohnson@cloudflare.com>
2024-10-26 18:24:50 +0200

    added add

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 use axum::{
-    routing::get,
-    Router,
+    extract::Path, routing::get, Router
 };
+
+async fn handle(
+    Path((a, b)): Path<(u32, u32)>,
+) -> String {
+    (a + b).to_string()
+}
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
     let app = Router::new().route("/", get(|| async { "Hello, World!" }))
-.route("/", get(|| async { "Hello, World!" }))
+.route("/add/:a/:b", get(handle));
         ;
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
